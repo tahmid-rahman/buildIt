430df14122b716bd92167dcd4483efd20c6b0af9
Sayem Ahmed <sahmed211030@bscse.uiu.ac.bd>
2024-07-07 18:22:36 +0600

    just checking

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="">
       <h1>hello world. this is wellcome page</h1>
+      <a href="thisissayem">hello</a>
     </main>
   );
 }
---
+++ src/app/thisissayem/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="">
+      <h1>hello world. this is Sayem Ahemd signing in </h1>
+    </main>
+  );
+}
