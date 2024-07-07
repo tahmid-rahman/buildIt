b04fc5139a1130315886ae6ec1d9cfb4f7ea2d73
tahmid-rahman <artahmid123@gmail.com>
2024-07-07 17:52:06 +0600

    login init commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,3 @@
+export default function login(){
+    return <h1> this is login page</h1>
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="bg-black h-screen text-white">
       <h1>hello world. this is wellcome page</h1>
+      <div className="mt-">
+      <a href="login" className="text-black px-3 py-1 rounded-md bg-white mt-10">join know</a>
+      </div>
     </main>
   );
 }
